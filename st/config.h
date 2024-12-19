c69edfc3d88a74323d0a5e2e3f97ab2f00803621
db <db@archimac>
2024-12-19 13:48:04 +0100

    Increased font size

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro NF:pixelsize=26:antialias=true:autohint=true";
-static int borderpx = 10;
+static char *font = "SauceCodePro NF:pixelsize=30:antialias=true:autohint=true";
+static int borderpx = 12;
 
 /*
  * What program is execed by st depends of these precedence rules:
