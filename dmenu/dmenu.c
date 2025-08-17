3af38c757f083235c6cfaf5a2b5214a42488ec4b
daan <contact@daanblom.com>
2025-08-17 14:32:46 +0200

    removed -P flag

--- dmenu/dmenu.c
+++ dmenu/dmenu.c
@@ -37,7 +37,7 @@ struct item {
 static char text[BUFSIZ] = "";
 static char *embed;
 static int bh, mw, mh;
-static int inputw = 0, promptw, passwd = 0;
+static int inputw = 0, promptw;
 static int lrpad; /* sum of left and right padding */
 static size_t cursor;
 static struct item *items = NULL;
@@ -146,11 +146,6 @@ static void drawmenu(void) {
   unsigned int curpos;
   struct item *item;
   int x = 0, y = 0, fh = drw->fonts->h, w;
-  char *censort;
-  if (passwd) {
-    inputw = lines = 0;
-    return;
-  }
 
   drw_setscheme(drw, scheme[SchemeNorm]);
   drw_rect(drw, 0, 0, mw, mh, 1, 1);
@@ -161,14 +156,9 @@ static void drawmenu(void) {
   }
   /* draw input field */
   w = (lines > 0 || !matches) ? mw - x : inputw;
-  drw_setscheme(drw, scheme[SchemeNorm]);
-  if (passwd) {
-    censort = ecalloc(1, sizeof(text));
-    memset(censort, '.', strlen(text));
-    drw_text(drw, x, 0, w, bh, lrpad / 2, censort, 0);
-    free(censort);
-  } else
-    drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
+	drw_setscheme(drw, scheme[SchemeNorm]);
+  /* drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);rw_text(drw, x, 0, w, bh, lrpad / 2, text, 0); */
+	drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
 
   curpos = TEXTW(text) - TEXTW(&text[cursor]);
   if ((curpos += lrpad / 2 - 1) < w) {
@@ -752,7 +742,7 @@ static void setup(void) {
 }
 
 static void usage(void) {
-  die("usage: dmenu [-bfivP] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+  die("usage: dmenu [-bfivc] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
       "monitor]\n"
       "             [-nb color] [-nf color] [-sb color] [-sf color] [-w "
       "windowid]");
@@ -776,9 +766,7 @@ int main(int argc, char *argv[]) {
     else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
       fstrncmp = strncasecmp;
       fstrstr = cistrstr;
-    } else if (!strcmp(argv[i], "-P")) /* is the input a password */
-      passwd = 1;
-    else if (i + 1 == argc)
+    } else if (i + 1 == argc)
       usage();
     /* these options take one argument */
     else if (!strcmp(argv[i], "-l")) /* number of lines in vertical list */
