78ef8355b1a835522c3ae7d647151302ef023909
Daan Blom <contact@daanblom.com>
2025-08-22 20:09:02 +0200

    updated ratio

--- dmenu/config.h
+++ dmenu/config.h
@@ -5,8 +5,8 @@
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static int centered = 0; /* -c option; centers dmenu on screen */
-static int min_width = 800; /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+static int min_width = 1000; /* minimum width when centered */
+static const float menu_height_ratio = 2.33f;  /* This is the ratio used in the original calculation */
 static const char *fonts[] = {"SauceCodePro NF:size=12"};
 static const char *prompt =  NULL; /* -p  option; prompt to the left of input field */
 #if __has_include(wal)
